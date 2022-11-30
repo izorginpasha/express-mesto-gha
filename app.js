3fb8fb0adabe7ddec57b71d016723e4146e40729
Izorgin Pavel <izorgin.pasha@yandex.ru>
2022-11-30 13:11:34 +0500

    db

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
-const express = require('express');
-
+const express = require('express');//
+const mongoose = require('mongoose');//
+// подключаемся к серверу mongo
+mongoose.connect('mongodb://localhost:27017/mestodb', {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+    useFindAndModify: false
+});
 // Слушаем 3000 порт
 const { PORT = 3000 } = process.env;
 const app = express();
+// подключаем мидлвары, роуты и всё остальное...
 
 
 app.listen(PORT, () => {
